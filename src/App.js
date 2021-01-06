d100d9e098281cc3c63a0a0fe556628b00eff6a5
schmidt peti <schmidt.peti32@gmail.com>
2021-01-06 19:22:09 +0100

    sign in buttons and special inputs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import {Route,Switch} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sing-out/sign-in-and-sing-out.component';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
-        <Route exact path='/shop' component={ShopPage}></Route>
+        <Route path='/shop' component={ShopPage}></Route>
+        <Route path='/signin' component={SignInAndSignUpPage}></Route>
       </Switch>
     </div>
   );
